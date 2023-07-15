d090ead136daa159f8f0564f65c53978f302b83c
ace <diamond-gold-ace@outlook.com>
2023-07-15 12:29:43 +0800

    Auto generate a simple readme for each plugin

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from urllib.request import Request, urlopen
 import json
 import os.path
+from datetime import datetime
 
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 
@@ -15,6 +16,9 @@ except:
     exit()
 with open('plugins.json','w') as f:
     f.write(json.dumps(plugins, indent=4))
+# with open('plugins.json','r') as f:
+    # jsonStr = f.read()
+    # plugins = json.loads(jsonStr)
 
 pm4 = []
 pm5 = []
@@ -28,6 +32,21 @@ for plugin in plugins:
     if plugin['api'][0]['from'][0:1] == '5':
         pm5.append(plugin)
 
+def generateReadme(plugin):
+    readme  = '# {:s}\n'.format(plugin['name'])
+    if plugin['icon_url'] is not None:
+        readme += '<img src="{:s}" width="128" height="128" />\n\n'.format(plugin['icon_url'])
+    readme += '## {:s}\n'.format(plugin['tagline'])
+    readme += '```properties\n'
+    readme += 'Version: {:s}\n'.format(plugin['version'])
+    readme += '    API: {:s}\n'.format(plugin['api'][0]['from'])
+    readme += '    Updated: {:s} UTC\n'.format(datetime.utcfromtimestamp(plugin['last_state_change_date']).strftime('%d-%m-%Y %H:%M:%S'))
+    readme += 'Repo: https://github.com/{:s}\n'.format(plugin['repo_name'])
+    readme += 'License: {:s}\n'.format(plugin['license'])
+    readme += 'Categories: {:s}\n'.format(','.join([cat['category_name'] for cat in plugin['categories']]))
+    readme += 'Keywords: {:s}\n'.format(','.join([words for words in plugin['keywords']]))
+    readme += '```'
+    return readme
 
 def download(base,plugins):
     print('{:s} {:d}'.format(base,len(plugins)))
@@ -66,6 +85,12 @@ def download(base,plugins):
             with urlopen(req) as f:
                 data = f.read().decode('utf-8')
             with open(changelog,'w', encoding='utf-8') as f:
+                f.write(data)
+        readme = path+'/README.md'
+        if not plugin['is_obsolete']:
+            # print('Generate readme {:s}'.format(progress))
+            data = generateReadme(plugin)
+            with open(readme,'w', encoding='utf-8') as f:
                 f.write(data)
 
 download('pm4',pm4)
